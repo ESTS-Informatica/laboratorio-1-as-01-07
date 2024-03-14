b94e09dfc88cf716c04c61fd599897ee2428a244
Alex202200037 <127303231+Alex202200037@users.noreply.github.com>
2024-03-14 10:20:15 +0000

    pinto

--- WordGuessingGame.java
+++ WordGuessingGame.java
@@ -2,8 +2,53 @@ public class WordGuessingGame {
     private String hiddenWord;
     private String guessedWord;
     private int numberOfTries;
+    private InputReader inputReader;
 
     public WordGuessingGame() {
+        this.hiddenWord = "abc";
+        this.guessedWord = "___";
+        this.numberOfTries = 0;
+        this.inputReader = new InputReader();
+    }
 
+    public void showWelcome() {
+        System.out.println("Olaaaaaaa");
+    }
+
+    public void showGuessedWord() {
+        System.out.println(guessedWord);
+    }
+
+    public void play() {
+        return;
+    }
+
+    public String getHiddenWord() {
+        return hiddenWord;
+    }
+
+    public void setHiddenWord(String hiddenWord) {
+        this.hiddenWord = hiddenWord;
+    }
+
+    public String getGuessedWord() {
+        return guessedWord;
+    }
+
+    public void setGuessedWord(String guessedWord) {
+        this.guessedWord = guessedWord;
+    }
+
+    public int getNumberOfTries() {
+        return numberOfTries;
+    }
+
+    public void setNumberOfTries(int numberOfTries) {
+        this.numberOfTries = numberOfTries;
+    }
+
+    public void guess() {
+        inputReader.getChar(guessedWord);
+    
     }
 }
